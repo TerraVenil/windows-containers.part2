d38c0f80271799263718de52b8f7936f1a502a33
Oleksii Borodenko <kosunix@gmail.com>
2020-03-12 12:36:40 +0200

    Add Demo2 examples.
    This reverts commit 0084ae4fd50ce9d5cdd533962b447fb058855087.

---
+++ Demo2/src/TechTalk.RedisIntegration.Entrypoint/Program.cs
@@ -0,0 +1,23 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Threading.Tasks;
+
+namespace RedisIntegrationTests.Entrypoint
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("redis:6379");
+
+            ISubscriber subscriber = redis.GetSubscriber();
+
+            while(true)
+            {
+                await subscriber.PublishAsync("techtalks:conversations", $"Say, hi!");
+
+                await Task.Delay(TimeSpan.FromSeconds(10));
+            }
+        }
+    }
+}
---
+++ Demo2/tests/TechTalk.RedisIntegration.Tests/SubscriberTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using StackExchange.Redis;
+using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RedisIntegrationTests
+{
+    public class SubscriberTests
+    {
+        [Fact]
+        public async Task SubscribeToRedis_ShouldRecieveMessages()
+        {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("redis:6379");
+            ConcurrentBag<string> messages = new ConcurrentBag<string>();
+
+            ISubscriber subscriber = redis.GetSubscriber();
+
+            await subscriber.SubscribeAsync("techtalks:conversations", (channel, message) =>
+            {
+                messages.Add(message);
+            });
+
+            await Task.Delay(TimeSpan.FromSeconds(5));
+
+            messages.Should().NotBeEmpty();
+        }
+    }
+}
