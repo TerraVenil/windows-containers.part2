68e36733a42939e990d326b10f5eced13c399e5b
Oleksii Borodenko <kosunix@gmail.com>
2020-03-19 10:57:27 +0200

    Add RDP vs SSH examples.

---
+++ Demo3/aws_rdp.ps1
@@ -0,0 +1,35 @@
+﻿$ENV_AWS_PRIV_LAUNCH_KEY = 'AWS_PRIV_LAUNCH_KEY'
+
+$awsPrivLaunchKeyPath    = 'env:\{0}' -f $ENV_AWS_PRIV_LAUNCH_KEY
+
+Try
+{
+    if (-Not (Test-Path $awsPrivLaunchKeyPath))
+    {
+        Write-Host 'Please specify $AWS_PRIV_LAUNCH_KEY to *.pem'
+    }
+    else
+    {
+        $scriptPath    = Split-Path $MyInvocation.InvocationName
+        $privLaunchKey = (Get-ChildItem -Path $awsPrivLaunchKeyPath).Value
+
+        Invoke-Expression -Command $scriptPath\aws_cli_configure.ps1
+        
+        $port                 = 3389
+
+        $instance             = (Get-EC2Instance -AccessKey $env:AWS_ACCESS_KEY_ID -SecretKey $env:AWS_SECRET_ACCESS_KEY -SessionToken $env:AWS_SESSION_TOKEN).Instances
+        $ipAddress            = $instance.PublicIpAddress
+        $password             = Get-EC2PasswordData -InstanceId $instance.InstanceId -PemFile $privLaunchKey
+
+        Write-Host "ContainerId" $containerId - $ipAddress - $password
+
+        cmdkey /generic:$ipAddress /user:Administrator /pass:$password
+
+        mstsc /v:${ipAddress}:$port
+    }
+}
+Catch
+{
+    $ErrorMessage = $_.Exception.Message
+    Write-Host "Failed to create RDP connection with message. $ErrorMessage"
+}
---
+++ Demo3/user-data.sh
@@ -0,0 +1,4 @@
+<powershell>
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+Start-Service sshd
+</powershell>
